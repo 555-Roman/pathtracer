8b86a918fd50a394b7fbda28cca07e8562bc206f
555-Roman <di.elle.vi11@gmail.com>
2026-08-10 13:18:45 +0200

    0.4.11 - camera relative movement and rotation

--- src/main.cpp
+++ src/main.cpp
@@ -34,8 +34,9 @@ GLuint textures[2];
 GLuint displayTexture;
 
 bool cameraRelativeMovement = false;
+bool cameraRelativeRotation = false;
 float MOVEMENT_SPEED = 1.0;
-float ROTATION_SPEED = 30.0;
+float ROTATION_SPEED = 90.0;
 vec3 cameraPos = vec3(2.17881, 2.43326, 3.91674);
 float cameraPitch = -32.5058;
 float cameraYaw = -34.9385;
@@ -323,6 +324,8 @@ int main() {
 
             ImGui::Begin("Camera");
             {
+                ImGui::Checkbox("Camera Relative Movement", &cameraRelativeMovement);
+                ImGui::Checkbox("Camera Relative Rotation", &cameraRelativeRotation);
 
                 ImGui::DragFloat("Movement Speed", &MOVEMENT_SPEED, 0.1);
                 ImGui::DragFloat("Rotation Speed", &ROTATION_SPEED, 0.1);
@@ -700,18 +703,34 @@ void processInput(GLFWwindow *window, float dt) {
     bool moved = false;
     bool rotated = false;
 
-    vec3 forwardMovement =
-        float((glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) - (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS))
-        * normalize(vec3(cameraForward.x, 0.0, cameraForward.z)) * dt * MOVEMENT_SPEED;
-    vec3 lateralMovement =
-        float((glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) - (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS))
-        * normalize(vec3(cameraRight.x, 0.0, cameraRight.z)) * dt * MOVEMENT_SPEED;
-    vec3 verticalMovement =
-        float((glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) - (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS))
-        * vec3(0.0, 1.0, 0.0) * dt * MOVEMENT_SPEED;
-    if (forwardMovement != vec3(0.0) || lateralMovement != vec3(0.0) || verticalMovement != vec3(0.0)) {
-        moved = true;
-        cameraPos += forwardMovement + lateralMovement + verticalMovement;
+    if (cameraRelativeMovement) {
+        vec3 forwardMovement =
+            float((glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) - (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS))
+            * cameraForward * dt * MOVEMENT_SPEED;
+        vec3 lateralMovement =
+            float((glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) - (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS))
+            * cameraRight * dt * MOVEMENT_SPEED;
+        vec3 verticalMovement =
+            float((glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) - (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS))
+            * cameraUp * dt * MOVEMENT_SPEED;
+        if (forwardMovement != vec3(0.0) || lateralMovement != vec3(0.0) || verticalMovement != vec3(0.0)) {
+            moved = true;
+            cameraPos += forwardMovement + lateralMovement + verticalMovement;
+        }
+    } else {
+        vec3 forwardMovement =
+            float((glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) - (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS))
+            * normalize(vec3(cameraForward.x, 0.0, cameraForward.z)) * dt * MOVEMENT_SPEED;
+        vec3 lateralMovement =
+            float((glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) - (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS))
+            * normalize(vec3(cameraRight.x, 0.0, cameraRight.z)) * dt * MOVEMENT_SPEED;
+        vec3 verticalMovement =
+            float((glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) - (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS))
+            * vec3(0.0, 1.0, 0.0) * dt * MOVEMENT_SPEED;
+        if (forwardMovement != vec3(0.0) || lateralMovement != vec3(0.0) || verticalMovement != vec3(0.0)) {
+            moved = true;
+            cameraPos += forwardMovement + lateralMovement + verticalMovement;
+        }
     }
 
     float pitchDelta =
@@ -723,16 +742,33 @@ void processInput(GLFWwindow *window, float dt) {
     float rollDelta =
         ((glfwGetKey(window, GLFW_KEY_PERIOD) == GLFW_PRESS) - (glfwGetKey(window, GLFW_KEY_COMMA) == GLFW_PRESS))
         * dt * ROTATION_SPEED;
-    if (pitchDelta != 0.0 || yawDelta != 0.0 || rollDelta != 0.0) {
-        rotated = true;
-        cameraPitch += pitchDelta;
-        cameraYaw += yawDelta;
-        cameraRoll += rollDelta;
+    if (cameraRelativeRotation) {
+        if (pitchDelta != 0.0 || yawDelta != 0.0 || rollDelta != 0.0) {
+            rotated = true;
 
-        cameraRotation = getRotationMatrix(cameraYaw, cameraPitch, cameraRoll);
-        cameraForward = cameraRotation * FORWARD;
-        cameraRight = cameraRotation * RIGHT;
-        cameraUp = cameraRotation * UP;
+            mat3 localRotation = getRotationMatrix(yawDelta, pitchDelta, rollDelta);
+            cameraRotation *= localRotation;
+
+            cameraForward = cameraRotation * FORWARD;
+            cameraRight = cameraRotation * RIGHT;
+            cameraUp = cameraRotation * UP;
+
+            cameraPitch = degrees(asin(clamp(-cameraRotation[2][1], -1.0f, 1.0f)));
+            cameraYaw = degrees(atan2(-cameraRotation[2][0], cameraRotation[2][2]));
+            cameraRoll = degrees(atan2(cameraRotation[1][0], cameraRotation[1][1]));
+        }
+    } else {
+        if (pitchDelta != 0.0 || yawDelta != 0.0 || rollDelta != 0.0) {
+            rotated = true;
+            cameraPitch += pitchDelta;
+            cameraYaw += yawDelta;
+            cameraRoll += rollDelta;
+
+            cameraRotation = getRotationMatrix(cameraYaw, cameraPitch, cameraRoll);
+            cameraForward = cameraRotation * FORWARD;
+            cameraRight = cameraRotation * RIGHT;
+            cameraUp = cameraRotation * UP;
+        }
     }
 
     if (moved || rotated) {
